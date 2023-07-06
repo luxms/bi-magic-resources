8ae270e0d0bbbac71c74ec64aadd5852d8222661
JannaRudyk <138606719+JannaRudyk@users.noreply.github.com>
2023-07-06 18:59:24 +0300

    test component

---
+++ src/ds_3/MyComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MyComponents.scss';
+class Test extends React.Component<any> {
+ constructor(props) {
+ super(props);
+ }
+ public render() {
+ return (
+ <div>My test component 06/07</div>
+ );
+ }
+ }
+ export default Test; 
